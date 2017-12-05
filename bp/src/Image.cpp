15fb853d90870cc35f67b1b1422e43840923344b
larso0 <larsolavtolo@gmail.com>
2017-12-05 16:30:01 +0100

    Using device local host visible memory for staging buffer if available.

--- bp/src/Image.cpp
+++ bp/src/Image.cpp
@@ -127,8 +127,9 @@ void* Image::map(VkDeviceSize offset, VkDeviceSize size)
 			stagingBuffer = new Buffer(device, memorySize,
 						   VK_BUFFER_USAGE_TRANSFER_SRC_BIT |
 						   VK_BUFFER_USAGE_TRANSFER_DST_BIT,
-						   VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-						   VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+						   VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
+						   VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT |
+						   VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
 		}
 		mappedMemory = stagingBuffer->map(offset, size);
 	}
